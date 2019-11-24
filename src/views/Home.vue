dafc1c641404bee65366bcf7d7b115294e0f6291
Ezekiel <iamwebwiz@gmail.com>
2019-11-25 00:56:22 +0100

    Added bootstrap to project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div class="container">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <h2>CustomerBook</h2>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  name: "home"
 };
 </script>
