50ebc80670f58c4934ba390afdd8515de0aa3df6
Ezekiel <iamwebwiz@gmail.com>
2019-11-25 15:58:01 +0100

    Updated CustomerBookService.js

--- src/services/CustomerBookService.js
+++ src/services/CustomerBookService.js
@@ -19,6 +19,48 @@ const CustomerBookService = {
         } catch (error) {
             throw new error;
         }
+    },
+    fetchSingleCustomer: async function (customerId) {
+        try {
+            const response = await window.axios.get(`${config.baseUrl}/customer`, {
+                params: {
+                    customerId: customerId
+                }
+            });
+
+            return response.data;
+        } catch (error) {
+            throw new error;
+        }
+    },
+    updateCustomer: async function (customer) {
+        try {
+            const response = await window.axios.put(`${config.baseUrl}/update`, {
+                customerId: customer.id,
+                first_name: customer.first_name,
+                last_name: customer.last_name,
+                email: customer.email,
+                phone: customer.phone,
+                address: customer.address
+            });
+
+            return response.data;
+        } catch (error) {
+            throw new error;
+        }
+    },
+    deleteCustomer: async function (customerId) {
+        try {
+            const response = await window.axios.delete(`${config.baseUrl}/delete`, {
+                params: {
+                    customerId: customerId
+                }
+            });
+
+            return response.data;
+        } catch (error) {
+            throw new error;
+        }
     }
 };
 
