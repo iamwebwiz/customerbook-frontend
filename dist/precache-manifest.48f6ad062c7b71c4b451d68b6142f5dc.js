668ec84c1ecda93264a94999383d19a36fddae90
Ezekiel <iamwebwiz@gmail.com>
2019-11-25 16:18:33 +0100

    Build app for production

---
+++ dist/precache-manifest.48f6ad062c7b71c4b451d68b6142f5dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61958009cdcc2a092d43",
+    "url": "/css/app.ca16e432.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "db3d0e1cf9c8c757866a429c368d002c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61958009cdcc2a092d43",
+    "url": "/js/app.10de9d9d.js"
+  },
+  {
+    "revision": "832e1b998bae4ccb830e",
+    "url": "/js/chunk-vendors.de7dee2c.js"
+  },
+  {
+    "revision": "5fd514f1c2823f7d1eb3a41435b65b54",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
