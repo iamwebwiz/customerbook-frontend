b259379e636382932dcaddb332a40256393d4b2b
Ezekiel <iamwebwiz@gmail.com>
2019-11-25 15:57:45 +0100

    Added edit customer modal component

--- src/components/EditCustomer.vue
+++ src/components/EditCustomer.vue
@@ -1,10 +1,50 @@
 <template>
-    
+    <div>
+        <div class="modal fade" id="editCustomerModal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">EDIT CUSTOMER</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="firstName">First Name</label>
+                            <input type="text" class="form-control" id="firstName" v-model="customer.first_name">
+                        </div>
+                        <div class="form-group">
+                            <label for="lastName">Last Name</label>
+                            <input type="text" class="form-control" id="lastName" v-model="customer.last_name">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="text" class="form-control" id="email" v-model="customer.email">
+                        </div>
+                        <div class="form-group">
+                            <label for="phone">Phone</label>
+                            <input type="tel" id="phone" class="form-control" v-model="customer.phone">
+                        </div>
+                        <div class="form-group">
+                            <label for="address">Address</label>
+                            <input type="text" class="form-control" id="address" v-model="customer.address">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" @click="onCustomerUpdate(customer)">Save</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "EditCustomer"
+        name: "EditCustomer",
+        props: ['customer', 'onCustomerUpdate']
     }
 </script>
 
